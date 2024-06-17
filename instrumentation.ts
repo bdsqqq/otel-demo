743224d86a735ef6e486a7622b8f6521c0e7c393
Igor Bedesqui <igorbedesqui@gmail.com>
2024-06-17 17:11:31 +0100

    what if I use the vercel package?

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,24 +1,45 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { Resource } from '@opentelemetry/resources';
-import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  const sdk = new NodeSDK({
-    resource: new Resource({
-      [SEMRESATTRS_SERVICE_NAME]: 'nextjs-app',
-    }),
-    spanProcessor: new SimpleSpanProcessor(
-      new OTLPTraceExporter({
-        url: 'https://api.axiom.co/v1/traces',
-        headers: {
-          Authorization: `Bearer ${process.env.API_TOKEN}`,
-          'X-Axiom-Dataset': process.env.DATASET_NAME,
-        },
-      })
-    ),
-  });
-
-  sdk.start();
+	registerOTel({
+		serviceName: "nextjs-app",
+		spanProcessors: [
+			new SimpleSpanProcessor(
+				new OTLPTraceExporter({
+					url: "https://api.axiom.co/v1/traces",
+					headers: {
+						"Authorization": `Bearer ${process.env.API_TOKEN}`,
+						"X-Axiom-Dataset": `${process.env.DATASET_NAME}`,
+					},
+				}),
+			),
+		],
+	});
 }
+
+// import { NodeSDK } from '@opentelemetry/sdk-node';
+// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+// import { Resource } from '@opentelemetry/resources';
+// import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+// import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
+
+// export function register() {
+//   const sdk = new NodeSDK({
+//     resource: new Resource({
+//       [SEMRESATTRS_SERVICE_NAME]: 'nextjs-app',
+//     }),
+//     spanProcessor: new SimpleSpanProcessor(
+//       new OTLPTraceExporter({
+//         url: 'https://api.axiom.co/v1/traces',
+//         headers: {
+//           Authorization: `Bearer ${process.env.API_TOKEN}`,
+//           'X-Axiom-Dataset': process.env.DATASET_NAME,
+//         },
+//       })
+//     ),
+//   });
+
+//   sdk.start();
+// }
