c5decf98bb15357f88e5ea8bcac4b55d403c4886
Igor Bedesqui <igorbedesqui@gmail.com>
2024-06-17 16:58:48 +0100

    don't cache the dice roll, lol

--- src/app/api/rolldice/route.ts
+++ src/app/api/rolldice/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 function getRandomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min) + min);
 }
